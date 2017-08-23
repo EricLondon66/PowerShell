3df89d10c6c72bdba84589956721c6117e1c0f28
EricLondon66 <EricLondon66@users.noreply.github.com>
2017-08-23 16:01:17 +0100

    Add files via upload

---
+++ Pester/HelloWorldExample/Get-HelloWorld.Tests.ps1
@@ -0,0 +1,44 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Get-HelloWorld" {
+    It "outputs 'Hello world!'" {
+        Get-HelloWorld | Should Be 'Hello world!'
+    }
+}
+
+Describe "Get Dir1" {
+    $result = Test-Path -Path C:\Temp\Dir1
+    It "outputs 'Dir1 Works'" {
+        $result | Should be $true
+    }
+}
+
+Describe "Get Dir2" {
+    $result = Test-Path -Path C:\Temp\Dir2
+    It "outputs 'Dir2 Works'" {
+        $result | Should be $true
+    }
+}
+
+Describe "Get Dir3" {
+    $result = Test-Path -Path C:\Temp\Dir3
+    It "outputs 'Dir3 Works'" {
+        $result | Should be $true
+    }
+}
+
+Describe "Get Dir4" {
+    $result = Test-Path -Path C:\Temp\Dir4
+    It "outputs 'Dir4 Works'" {
+        $result | Should be $true
+    }
+}
+
+Describe "Get Dir5" {
+    $result = Test-Path -Path C:\Temp\Dir5
+    It "outputs 'Dir5 Works'" {
+        $result | Should be $true
+    }
+}
